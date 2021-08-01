c4250e954c1f1dfc43323d70c531fecc433150e9
Anurag Singh <72293742+Anurag-Singh-Rajput@users.noreply.github.com>
2021-08-01 11:29:27 +0530

    homepage fix

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import vectorPortrait from "../images/avatar-green.png";
 function Home(){
     return(
-        <div className = "home-section">
-            <h1>Hello</h1>
-            <h1>I am Anurag</h1>
-            
-            <img className = "avatar-img" alt = "avatar-img" src = {vectorPortrait} />
+        <div className = "home-section container-fluid">
+        <div class="row">
+      <div class="col-lg-6">
+        <div className = "home-text"><h3>Hello !!</h3>
+        <h1 >I am Anurag !!</h1>
+        <p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint dicta fugiat incit consectetur, adipisicing elit. Quas, recusandae. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, possimus. uo culpa animi, molestias quod esse officiis velit!</p>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <img class="avatar-img" src= {vectorPortrait} alt="avatar-img" />
+      </div>
+    </div>
         </div>
     );
 }
